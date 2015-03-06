51068e646152ff0f5e546d0047719dc34f25637e
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-03-06 12:03:38 +0000

    SkilledRouter to release operators if needed

--- dream/simulation/SkilledOperatorRouter.py
+++ dream/simulation/SkilledOperatorRouter.py
@@ -178,6 +178,32 @@ class SkilledRouter(Router):
                 self.pendingStations=[]
                 from Globals import findObjectById
                 # apply the solution
+                
+                # loop through the stations. If there is a station that should change operator
+                # set the operator dedicated to None and also release operator
+                for station in G.MachineList:
+                    # obtain the operator and the station
+                    if station.currentOperator:
+                        operator=station.currentOperator
+                        previousAssignment=self.previousSolution.get(operator.id,None)
+                        currentAssignment=solution.get(operator.id,None)
+                        if (not previousAssignment == currentAssignment):
+                            operator.operatorDedicatedTo=None
+                            if not station.isProcessing:
+#                                 print self.env.now, station.id, 'releases', operator.id
+                                station.releaseOperator()
+                    
+#                     print operatorID
+#                     if operatorID in self.previousSolution:
+#                         # if the solution returned the operator that is already in the station
+#                         # then no signal is needed
+#                         print 1
+#                         if not self.previousSolution[operatorID] == solution[operatorID]:
+#                             print station.name,station.isProcessing,station.currentOperator
+#                             if (not station.isProcessing) and station.currentOperator:
+#                                 print self.env.now, station.id, 'releases', operator.id
+#                                 station.releaseOperator()  
+                            
                 # loop through the entries in the solution dictionary
                 for operatorID in solution.keys():
                     # obtain the operator and the station
